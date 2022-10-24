6f51e02fe7a44b58b7264201546709a706ad5728
marfgold1 <13520103@std.stei.itb.ac.id>
2022-10-24 17:51:03 +0700

    func: add database class

---
+++ src/core/database.php
@@ -0,0 +1,35 @@
+<?php
+namespace MusicApp\Core;
+
+use PDO;
+
+class Database extends PDO {
+    protected static array $cache_config;
+
+    public function __construct(array $conf) {
+        parent::__construct($conf['dsn'], $conf['username'], $conf['password']);
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public static function init() : void {
+        static::$cache_config = [
+            'dsn' => $_ENV["DB_NS"] ?? (
+                "mysql:host=" . ($_ENV["DB_SERVER"] ?? "localhost") .
+                ";port=" . ($_ENV["DB_PORT"] ?? 3306) .
+                ";dbname=" . ($_ENV["DB_NAME"] ?? "musicphpapp")
+            ),
+            'username' => $_ENV["DB_USER"] ?? "musicphpapp",
+            'password' => $_ENV["DB_PASSWORD"] ?? "musicenjoyer"
+        ];
+    }
+
+    public static function get($config=null) : Database {
+        if (!is_null($config)) {
+            static::$cache_config = $config;
+        }
+        return new Database(static::$cache_config);
+    }
+}
+Database::init();
+
+?>
