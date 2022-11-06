d5d168d86f4dd8fb370cf17fe4749d71a787152b
marfgold1 <13520103@std.stei.itb.ac.id>
2022-11-06 10:15:57 +0700

    fix: home crashed because no null check before sorting on songs

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -9,7 +9,8 @@ use function MusicApp\Core\view;
 class HomeController extends Controller {
     public function home() {
         $songs = Song::find('', [], 'ORDER BY song_id DESC LIMIT 10');
-        usort($songs, fn($a, $b) => strcmp($a->judul, $b->judul));
+        if ($songs)
+            usort($songs, fn($a, $b) => strcmp($a->judul, $b->judul));
         view('home', ['songs' => $songs]);
     }
 }
